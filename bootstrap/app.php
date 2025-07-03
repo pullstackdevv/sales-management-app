53a7778f92909f572a722fb587752eccf65daae8
fnhame29 <fnhame29@gmail.com>
2025-07-03 17:49:47 +0800

    feat: Remove unused middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Helpers\ResponseFormatter;
+use Illuminate\Cache\RateLimiter;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware
             ->group('api', [
                 EnsureFrontendRequestsAreStateful::class,
-                ThrottleRequests::class . ':api',
                 SubstituteBindings::class,
             ]);
     })
@@ -38,4 +38,5 @@ return Application::configure(basePath: dirname(__DIR__))
                 );
             }
         });
-    })->create();
+    })
+    ->create();

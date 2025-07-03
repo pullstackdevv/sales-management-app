ca71564ee778132d97378f50805340b233b56e79
fnhame29 <fnhame29@gmail.com>
2025-07-03 15:09:34 +0800

    feat: Enhance response formatter for message bag and throwable

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -2,6 +2,9 @@
 
 namespace App\Helpers;
 
+use Illuminate\Contracts\Support\MessageBag;
+use Throwable;
+
 class ResponseFormatter
 {
     public static function success(string $message = 'Success', mixed $data = null, int $statusCode = 200)
@@ -16,16 +19,31 @@ class ResponseFormatter
 
     public static function fail(string $message = 'Fail', array|object $errors = [], int $statusCode = 422)
     {
+        if ($errors instanceof MessageBag || method_exists($errors, 'toArray')) {
+            $errors = self::formatValidationErrors($errors);
+        }
+
         return self::format([
-            'status'  => 'fail',
+            'status' => 'fail',
             'message' => $message,
-            'data'    => null,
-            'errors'  => $errors,
+            'data' => null,
+            'errors' => $errors,
         ], $statusCode);
     }
 
     public static function error(string $message = 'Server Error', array|object $errors = [], int $statusCode = 500)
     {
+        if ($errors instanceof Throwable) {
+            $errors = [
+                [
+                    'field' => 'exception',
+                    'tag' => null,
+                    'message' => config('app.debug') ?
+                        $errors->getMessage() : 'Something went wrong.',
+                ]
+            ];
+        }
+
         return self::format([
             'status'  => 'error',
             'message' => $message,
@@ -34,9 +52,46 @@ class ResponseFormatter
         ], $statusCode);
     }
 
-    /**
-     * Fungsi utama response formatter
-     */
+    private static function formatValidationErrors($errors): array
+    {
+        $formatted = [];
+
+        foreach ($errors->toArray() as $field => $messages) {
+            foreach ($messages as $msg) {
+                $formatted[] = [
+                    'field' => $field,
+                    'tag' => self::extractRuleFromMessage($msg),
+                    'message' => $msg,
+                ];
+            }
+        }
+
+        return $formatted;
+    }
+
+    private static function extractRuleFromMessage(string $message): string
+    {
+        $rules = [
+            'required',
+            'email',
+            'max',
+            'min',
+            'string',
+            'numeric',
+            'unique',
+            'confirmed'
+        ];
+
+        // check from messsage
+        foreach ($rules as $rule) {
+            if (str_contains(strtolower($message), $rule)) {
+                return $rule;
+            }
+        }
+
+        return 'invalid';
+    }
+
     private static function format(array $body, int $statusCode)
     {
         return response()->json($body, $statusCode);

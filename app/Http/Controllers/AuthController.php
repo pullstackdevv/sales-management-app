2b1a3037bf989fc6c7d377aa929df0476702243f
fnhame29 <fnhame29@gmail.com>
2025-07-03 17:43:09 +0800

    feat: Add register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ResponseFormatter;
-// use Illuminate\Contracts\Validation\Validator;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -48,10 +50,60 @@ class AuthController extends Controller
             );
         }
 
-
         return ResponseFormatter::success(
             message: "Successfully logged in",
             data: Auth::user(),
+        );
+    }
+
+    /**
+     * Handle user registration.
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'min:6', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::fail(
+                "Invalid validation",
+                $validator->errors(),
+            );
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $validated = $validator->validate();
+
+            $name = $validated['name'];
+            $email = $validated['email'];
+            $password = $validated['password'];
+
+            $user = User::create([
+                'name'     => $name,
+                'email'    => $email,
+                'password' => Hash::make($password),
+            ]);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return ResponseFormatter::error("Something wrong", $th);
+        }
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ResponseFormatter::success(
+            message: "User successfully registered",
+            data: [
+                'user' => $user,
+                'token' => $token,
+            ],
         );
     }
 

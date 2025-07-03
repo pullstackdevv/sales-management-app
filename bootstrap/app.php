da1621c09fea661bfe21b3738e46e0f4ef2ade15
fnhame29 <fnhame29@gmail.com>
2025-07-03 14:32:36 +0800

    feat: Add response formatter in exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\ResponseFormatter;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -23,5 +24,18 @@ return Application::configure(basePath: dirname(__DIR__))
             ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (Throwable $e, $request) {
+            if ($request->expectsJson()) {
+                return ResponseFormatter::error(
+                    'Server Error',
+                    [[
+                        'field' => null,
+                        'tag' => 'exception',
+                        'message' => config('app.debug') ? $e->getMessage() : 'Something went wrong.'
+                    ]],
+                    method_exists($e, 'getStatusCode') ?
+                        $e->getStatusCode() : 500
+                );
+            }
+        });
     })->create();

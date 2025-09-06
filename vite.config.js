c4fd23f520c3c6257faa209b0ba2125e67a9f773
alwayslucky16 <andjar.atmoko@gmail.com>
2025-09-07 03:32:09 +0700

    build: update base paths for production and development builds
    Modify the base paths to correctly point to build directories in both production and development environments

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/public/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/public/build/' : '/build/',
   //  server: {
   //   host: '0.0.0.0',
   //   port: 5173,

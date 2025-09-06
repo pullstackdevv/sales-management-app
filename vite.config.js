4ca8b13ceb4c74b7ad5814537c4d9508a13f96c6
alwayslucky16 <andjar.atmoko@gmail.com>
2025-09-07 02:41:57 +0700

    build: add base URL configuration for production env
    Set the base URL dynamically based on the environment to ensure proper asset loading in production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? process.env.VITE_APP_URL + '/' : '/',
   //  server: {
   //   host: '0.0.0.0',
   //   port: 5173,
